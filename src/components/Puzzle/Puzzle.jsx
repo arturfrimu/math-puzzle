a338dac74a3a9a7b03c5fc9ab2a548c8ed525552
Artur Frimu <artur.frimu@ussoftwaresolutions.us>
2022-12-25 11:34:52 +0200

    finish first part

--- src/components/Puzzle/Puzzle.jsx
+++ src/components/Puzzle/Puzzle.jsx
@@ -1,22 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from "./Puzzle.module.scss";
 
+const interval = {min: 2, max: 10}
+
+const randomIntFromInterval = ({min, max}) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const operators = ["+", "-"]
+const x = randomIntFromInterval(interval);
+
 const Puzzle = () => {
-  return (
-    <section className={classes["puzzle-sectino"]}>
-      <div className={classes.puzzle}>
-        <div className={classes.puzzle__board}>
-          <span>4 + 4 = ?</span>
-        </div>
-        <div className={classes.puzzle__answers}>
-          <button className={classes.puzzle__answer}>2</button>
-          <button className={classes.puzzle__answer}>6</button>
-          <button className={classes.puzzle__answer}>12</button>
-          <button className={classes.puzzle__answer}>8</button>
-        </div>
-      </div>
-    </section>
-  );
+    const [nums, setNums] = useState({x, y: randomIntFromInterval({...interval, max: x})})
+    const [answer, setAnswer] = useState("")
+    const [wrongAnswer, setWrongAnswer] = useState(false)
+    const [randomOperator, setRandomOperator] = useState(operators[randomIntFromInterval({
+        min: 0,
+        max: operators.length - 1
+    })])
+    const [equations, setEquations] = useState(JSON.parse(localStorage.getItem('equations')) || 0)
+
+    const generateEq = () => {
+        return `${nums.x} ${randomOperator} ${nums.y} = ${answer ? answer : "?"}`
+    }
+
+    function doAction() {
+        if (randomOperator === "+") {
+            return nums.x + nums.y;
+        } else if (randomOperator === "-") {
+            return nums.x - nums.y;
+        }
+
+    }
+
+    const checkAnswer = () => {
+        if (+answer === doAction()) {
+            const x = randomIntFromInterval(interval);
+            setNums({x, y: randomIntFromInterval({...interval, max: x})})
+            setWrongAnswer(false)
+            setRandomOperator(operators[randomIntFromInterval({min: 0, max: operators.length - 1})])
+            setEquations(prev => prev + 1)
+            localStorage.setItem('equations', JSON.stringify(equations + 1))
+        } else {
+            console.error('Wrong answer!!!')
+            setWrongAnswer(true)
+        }
+        setAnswer("")
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        checkAnswer()
+    }
+
+    const errorClass = wrongAnswer ? classes.incorrect : ""
+
+    return (
+        <section className={classes["puzzle-sectino"]}>
+            <div className={classes.puzzle}>
+                <div className={classes.puzzle__board}>
+                    <div className={classes.puzzle__equations}>Exercitii rezolvate: {equations}</div>
+                    <span>{generateEq()}</span>
+                </div>
+                <form className={classes.puzzle__answer} onSubmit={submitHandler}>
+                    <input type="number" onChange={e => setAnswer(e.target.value)} value={answer}
+                           className={errorClass} placeholder={wrongAnswer ? "Raspuns gresit" : ""}/>
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Puzzle;
